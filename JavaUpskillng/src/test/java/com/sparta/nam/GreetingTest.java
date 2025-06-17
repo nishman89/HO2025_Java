6de3874b0db20bcab6b5df98a915d5d9b59875d7
unknown <nish_mandal@hotmail.com>
2025-06-17 11:11:27 +0100

    Whatever

---
+++ JavaUpskillng/src/test/java/com/sparta/nam/GreetingTest.java
@@ -0,0 +1,29 @@
+package com.sparta.nam;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class GreetingTest {
+
+    @Test
+    @DisplayName("My first test")
+    public void firstTest(){
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    @DisplayName("Greeting returns good evening with input of 21")
+    public void greeting_ReturnsGoodEvening_input21(){
+        // Arrange
+        int time = 21;
+        String expected = "Good evening!";
+        // Act - call method under test
+        String actual = Main.greeting(time);
+
+        // Assert
+        Assertions.assertEquals(expected, actual);
+    }
+
+    // Write a test for good morning 
+}
